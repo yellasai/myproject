d480a7484afd45d34fe305cc965c2e89a95d2349
yellasai <yellasaisomasekhar@lyrostech.com>
2024-03-17 10:22:11 +0530

    n

---
+++ src/Components/Hero.js
@@ -0,0 +1,26 @@
+const Hero = () => {
+  return (
+    <main className="herocontainer">
+      <div className="herocontent">
+        <h1>YOUR FEET DESERVE THE BEST</h1>
+        <p>
+          YOUR FEET DESERVE THE BEST AND WE'RE HERE TO HELP YOU WITH OUR SHOES.
+          YOUR FEET DESERVE THE BEST AND WE'RE HERE TO HELP YOU WITH OUR SHOES.
+        </p>
+      
+      <div className="herobtn">
+        <button>Shop Now</button>
+        <button>Category</button>
+      </div>
+      </div>
+      <div className="shopping">
+        <p>Also Available On</p>
+      </div>
+      <div className="heroimage">
+        <img src="images/imageshoess.webp"  />
+      </div>
+    </main>
+  );
+};
+
+export default Hero;
---
+++ src/Components/Navigation.js
@@ -0,0 +1,21 @@
+const Navigation = () => {
+  return (
+    <div>
+      <nav className="container">
+        <div className="logo">
+          <img src="images/Image.png" />
+        </div>
+        <ul>
+          <li href="#">Menu</li>
+          <li href="#">Location</li>
+          <li href="#">About</li>
+          <li href="#">Contact</li>
+        </ul>
+
+        <button>Login</button>
+      </nav>
+    </div>
+  );
+};
+
+export default Navigation;
